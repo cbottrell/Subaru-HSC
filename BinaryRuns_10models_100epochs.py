9b09fa59c876a8092b32ba65c3657db292714da5
Connor Bottrell <bottrell@beluga2.int.ets1.calculquebec.ca>
2019-08-07 21:39:47 -0400

    Generated new augmented images and made 10 models.

--- BinaryRuns_10models_100epochs.py
+++ BinaryRuns_10models_100epochs.py
@@ -6,29 +6,18 @@ from keras import backend as K
 from keras.models import Model,load_model
 from keras.layers import Input,Dense,Dropout,Flatten
 from keras.utils import plot_model
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping,ModelCheckpoint,ReduceLROnPlateau
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
 from sklearn import model_selection as md
 from keras import regularizers
 import numpy as np
-from collections import Counter
 
 def save_model_history(model,history_filename):
     with open(history_filename,'w') as f:
         json.dump(model.history.history, f)
         
-##############################################################
-
-def balanced_class_weights(y):
-   counter = Counter(y)
-   majority = max(counter.values())
-   return  {cls: float(majority/count) for cls, count in counter.items()}
-
-
-#############################################################
-
 ######################################################################
 def HSC_Subaru_CNN(args):
     
@@ -75,44 +64,40 @@ def HSC_Subaru_CNN(args):
 classIDs = ['Negative','Positive']
 dataDir = '/home/bottrell/scratch/Subaru/HyperSuprime/Data/Binary/'
 modelDir = '/home/bottrell/scratch/Subaru/HyperSuprime/Models/Binary/'
-fileNames = ['{}{}_grizy_Images-AugNorm.npy'.format(dataDir,classID) for classID in classIDs]
-catNames = ['{}Negative_grizy_Images_cat.npy'.format(dataDir),'{}Positive_grizy_Images-Aug_cat.npy'.format(dataDir)]
+fileNames = ['{}{}_i_Images-RAugNorm.npy'.format(dataDir,classID) for classID in classIDs]
+catNames = ['{}{}_i_Images-RAug_cat.npy'.format(dataDir,classID) for classID in classIDs]
 
 tar = np.array([])
 inp = np.array([])
 # Negatives
-inp_neg = np.load(fileNames[0])[:,:,:,2]
-inp_neg[np.isnan(inp_neg)]=0.
+inp_neg = np.load(fileNames[0])[:10000]
+print('Negatives:',inp_neg.shape)
 # inp_neg = np.zeros(shape=(20010,128,128)) # !!! for testing
-cat_neg = np.load(catNames[0])
+cat_neg = np.load(catNames[0])[:10000]
 tar_neg = np.append(tar,np.ones(inp_neg.shape[0])*0)
 # Positives
-inp_pos = np.load(fileNames[1])[:1201,:,:,2]
-#inp_pos = np.ones(shape=(20417,128,128)) # !!! for testing
-# inp_pos[:,48:80,48:80] = 1. # !!! for testing
-cat_pos = np.load(catNames[1])[:1201]
-tar_pos = np.ones(inp_pos.shape[0])[:1201]
+inp_pos = np.load(fileNames[1])
+print('Positives:',inp_pos.shape)
+cat_pos = np.load(catNames[1])
+tar_pos = np.ones(inp_pos.shape[0])
 # Combining
 inp = np.concatenate([inp_neg,inp_pos],axis=0)
 cat = np.concatenate([cat_neg,cat_pos],axis=0)
 tar = np.append(tar_neg,tar_pos)
-# Reformatting
-#tar_OH = keras.utils.to_categorical(tar)
-inp = inp.reshape((*inp.shape,1))
 
 nRecords,dx,dy,nChannels = inp.shape
 nClasses=len(classIDs)
 #del new_inp,new_cat
 
-batch_size = 64
+batch_size = 32
 epochs=100
 train_valid_split = 0.3
 valid_test_split = 0.5
 update = 0
-version = 0
+version = 1
 label = 'HSC-Subaru'
           
-randomStates = [1,2,3,4,5,6,7,8,9,42]
+randomStates = np.arange(10)
 
 for randomState in randomStates:
 
@@ -124,26 +109,29 @@ for randomState in randomStates:
     print('Model will save as: {}'.format(modelFile))
     historyFile = historyFile.format(label,randomState,version,update)
     print('History will save as: {}'.format(historyFile))
+    
+    mcp_file = modelFile.replace('.h5','_wgts.hdf5')
+    early_stopping = EarlyStopping(monitor='val_acc', patience=999, verbose=1, mode='max')
+    mcp_save = ModelCheckpoint(mcp_file, save_best_only=True, monitor='val_acc', mode='max')
+    reduce_lr_loss = ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='max')
 
     # Split training and validation data
     inp_train, inp_valid, tar_train, tar_valid, cat_train, cat_valid = md.train_test_split(inp, tar, cat, test_size=train_valid_split, random_state=randomState)
     # Reserve some validation data as test data
     inp_valid, inp_test, tar_valid, tar_test, cat_valid, cat_test = md.train_test_split(inp_valid, tar_valid, cat_valid, test_size=valid_test_split, random_state=randomState)
 
-    print(inp_train.shape)
     args = (dx,dy,nChannels)
     model = HSC_Subaru_CNN(args)
     
     model.summary()
-    
-    class_weight=balanced_class_weights(tar_train)
-    
-    print("Weights used:", class_weight)
-    
-    early_stopping = EarlyStopping(monitor='val_loss',patience=999,verbose=0,mode='auto')
+
     model.fit(inp_train,tar_train,shuffle=True,epochs=epochs,
               batch_size=batch_size,validation_data=[inp_valid,tar_valid],
-              callbacks=[early_stopping],class_weight = class_weight) 
+              callbacks=[early_stopping, mcp_save]) 
     
-    model.save(modelFile)
     save_model_history(model=model,history_filename=historyFile)
+    if os.access(mcp_file,0):
+        model.load_weights(mcp_file)
+        os.remove(mcp_file)
+    model.save(modelFile)
+
