d19547a9672c8abdec02651874c526eda5297fbf
Connor Bottrell <bottrell@beluga4.int.ets1.calculquebec.ca>
2019-08-14 20:09:24 -0400

    Major updates to augmentation/training.

--- BinaryRuns_10models_100epochs.py
+++ BinaryRuns_10models_100epochs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#!/usr/bin/env python
+
 import os,sys,json
 import keras
 from keras import backend as K
@@ -49,9 +51,9 @@ def HSC_Subaru_CNN(args):
 
     # Fully Connected Layer
     x = Dense(64,activation='relu',name='Dense_1')(x)
-    x = Dropout(0.25,name='DropFCL_1')(x)
+    x = Dropout(0.5,name='DropFCL_1')(x)
     x = Dense(16,activation='relu',name='Dense_2')(x)
-    x = Dropout(0.25,name='DropFCL_2')(x)
+    x = Dropout(0.5,name='DropFCL_2')(x)
     x = Dense(1,activation='sigmoid',name='Dense_3')(x)
 
     # connect and compile
@@ -61,40 +63,110 @@ def HSC_Subaru_CNN(args):
     return model
 ######################################################################
 
+def split_data(inp,tar,cat,n_negOrig,n_neg,n_posOrig,n_pos,
+               train_valid_split=0.3,valid_test_split=0.5,randomState=0):
+    
+    f_negAug = int(n_neg/n_negOrig)-1
+    f_posAug = int(n_pos/n_posOrig)-1
+
+    # indices of the original pre-augmentation images
+    indices_neg0 = np.arange(n_negOrig).astype(int)
+    indices_pos0 = np.arange(n_posOrig).astype(int)+n_neg
+
+    # this randomstate ensures we would have the same images if we did this again
+    randomState = 0
+    indices_neg_train0,indices_neg_valid0 = md.train_test_split(indices_neg0,test_size=train_valid_split, random_state=randomState)
+    indices_neg_valid0,indices_neg_test0  = md.train_test_split(indices_neg_valid0,test_size=valid_test_split, random_state=randomState)
+    indices_pos_train0,indices_pos_valid0 = md.train_test_split(indices_pos0,test_size=train_valid_split, random_state=randomState)
+    indices_pos_valid0,indices_pos_test0  = md.train_test_split(indices_pos_valid0,test_size=valid_test_split, random_state=randomState)
+
+    indices_neg_train = np.array([]).astype(int)
+    indices_neg_valid = np.array([]).astype(int)
+    indices_neg_test = np.array([]).astype(int)
+    indices_pos_train = np.array([]).astype(int)
+    indices_pos_valid = np.array([]).astype(int)
+    indices_pos_test = np.array([]).astype(int)
+
+    # now incorporate augmented images into each dataset
+    for i in range(f_negAug+1):
+        indices_neg_train = np.concatenate([indices_neg_train,indices_neg_train0+i*n_negOrig])
+        indices_neg_valid = np.concatenate([indices_neg_valid,indices_neg_valid0+i*n_negOrig])
+        indices_neg_test = np.concatenate([indices_neg_test,indices_neg_test0+i*n_negOrig])
+    for i in range(f_posAug+1):
+        indices_pos_train = np.concatenate([indices_pos_train,indices_pos_train0+i*n_posOrig])
+        indices_pos_valid = np.concatenate([indices_pos_valid,indices_pos_valid0+i*n_posOrig])
+        indices_pos_test = np.concatenate([indices_pos_test,indices_pos_test0+i*n_posOrig])
+    print('Negatives:', len(indices_neg_train),len(indices_neg_valid),len(indices_neg_test))
+    print('Positives:', len(indices_pos_train),len(indices_pos_valid),len(indices_pos_test))
+
+    # join
+    indices_train = np.concatenate([indices_neg_train,indices_pos_train])
+    indices_valid = np.concatenate([indices_neg_valid,indices_pos_valid])
+    indices_test = np.concatenate([indices_neg_test,indices_pos_test])
+
+    # # Now apply to full data matrix
+    tar_train = tar[indices_train]
+    inp_train = inp[indices_train]
+    cat_train = cat[indices_train]
+    tar_valid = tar[indices_valid]
+    inp_valid = inp[indices_valid]
+    cat_valid = cat[indices_valid]
+    tar_test = tar[indices_test]
+    inp_test = inp[indices_test]
+    cat_test = cat[indices_test]
+    return inp_train,tar_train,cat_train,inp_valid,tar_valid,cat_valid,inp_test,tar_test,cat_test
+
+
+
 classIDs = ['Negative','Positive']
-dataDir = '/home/bottrell/scratch/Subaru/HyperSuprime/Data/Binary/'
 modelDir = '/home/bottrell/scratch/Subaru/HyperSuprime/Models/Binary/'
-fileNames = ['{}{}_i_Images-RAugNorm.npy'.format(dataDir,classID) for classID in classIDs]
-catNames = ['{}{}_i_Images-RAug_cat.npy'.format(dataDir,classID) for classID in classIDs]
+dataDir = '/home/bottrell/scratch/Subaru/HyperSuprime/Data/Binary/'
+fileNames = ['{}{}_i_Images-RZSAugNorm.npy'.format(dataDir,classID) for classID in classIDs]
+catNames = ['{}{}_i_Images-RZSAug_cat.npy'.format(dataDir,classID) for classID in classIDs]
+
+np.random.seed(0)
+negative_subsample = 10000
 
 tar = np.array([])
 inp = np.array([])
 # Negatives
-inp_neg = np.load(fileNames[0])[:10000]
+#indices_neg = np.random.choice(np.arange(negative_total),negative_samples,replace=False)
+inp_neg = np.load(fileNames[0])[:negative_subsample] # [indices_neg]
 print('Negatives:',inp_neg.shape)
-# inp_neg = np.zeros(shape=(20010,128,128)) # !!! for testing
-cat_neg = np.load(catNames[0])[:10000]
-tar_neg = np.append(tar,np.ones(inp_neg.shape[0])*0)
+n_neg = inp_neg.shape[0]
+n_negOrig = 5000
+f_negAug = int(n_neg/n_negOrig)-1
+print('Number of augmentations per original image:', f_negAug)
+tar_neg = np.append(tar,np.ones(n_neg)*0)
+cat_neg = np.load(catNames[0])[:negative_subsample]
+
+
 # Positives
 inp_pos = np.load(fileNames[1])
 print('Positives:',inp_pos.shape)
+n_pos = inp_pos.shape[0]
+n_posOrig = 1201
+f_posAug = int(n_pos/n_posOrig)-1
+print('Number of augmentations per original image:', f_posAug)
+tar_pos = np.ones(n_pos)
 cat_pos = np.load(catNames[1])
-tar_pos = np.ones(inp_pos.shape[0])
 # Combining
-inp = np.concatenate([inp_neg,inp_pos],axis=0)
-cat = np.concatenate([cat_neg,cat_pos],axis=0)
+inp = np.concatenate([inp_neg,inp_pos],axis=0).reshape(-1,128,128,1)
 tar = np.append(tar_neg,tar_pos)
+cat = np.concatenate([cat_neg,cat_pos],axis=0)
+
 
 nRecords,dx,dy,nChannels = inp.shape
 nClasses=len(classIDs)
 #del new_inp,new_cat
 
 batch_size = 32
-epochs=100
+epochs=30
 train_valid_split = 0.3
 valid_test_split = 0.5
 update = 0
-version = 1
+version = 3
+patience = 999
 label = 'HSC-Subaru'
           
 randomStates = np.arange(10)
@@ -111,17 +183,15 @@ for randomState in randomStates:
     print('History will save as: {}'.format(historyFile))
     
     mcp_file = modelFile.replace('.h5','_wgts.hdf5')
-    early_stopping = EarlyStopping(monitor='val_acc', patience=999, verbose=1, mode='max')
+    early_stopping = EarlyStopping(monitor='val_acc', patience=patience, verbose=1, mode='max')
     mcp_save = ModelCheckpoint(mcp_file, save_best_only=True, monitor='val_acc', mode='max')
     reduce_lr_loss = ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='max')
 
-    # Split training and validation data
-    inp_train, inp_valid, tar_train, tar_valid, cat_train, cat_valid = md.train_test_split(inp, tar, cat, test_size=train_valid_split, random_state=randomState)
-    # Reserve some validation data as test data
-    inp_valid, inp_test, tar_valid, tar_test, cat_valid, cat_test = md.train_test_split(inp_valid, tar_valid, cat_valid, test_size=valid_test_split, random_state=randomState)
-
     args = (dx,dy,nChannels)
     model = HSC_Subaru_CNN(args)
+    
+    inp_train,tar_train,cat_train,inp_valid,tar_valid,cat_valid,inp_test,tar_test,cat_test = split_data(inp,tar,cat,
+                                                                                                       5000,10000,1201,9608,randomState=randomState)
     
     model.summary()
 
